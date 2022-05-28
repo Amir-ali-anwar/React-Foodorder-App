b8fdf7e94d7decb05bf310d21d59759e893200dd
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-05-28 11:14:46 +0500

    added useref and forward ref for getting the number from input

--- src/components/Meals/MealsItem/MealItemForm.js
+++ src/components/Meals/MealsItem/MealItemForm.js
@@ -1,13 +1,28 @@
-import React from 'react'
-import classes from './MealItemForm.module.css'
-import Input from '../../UI/Input'
+import React from "react";
+import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
 const MealItemForm = (props) => {
+  const amountRef = React.useRef();
+  const SubmitHandler = (e) => {
+    e.preventDefault();
+    console.log("clicked");
+  };
   return (
-    <form className={classes.form}>
-        <Input label="Amount" input={{id:'amount', type:"number", min:'1',max:'5' , defaultValue:"1"}}/>
-        <button>+Add</button>
+    <form className={classes.form} onSubmit={SubmitHandler}>
+      <Input
+        ref={amountRef}
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          defaultValue: "1",
+        }}
+      />
+      <button>+Add</button>
     </form>
-  )
-}
+  );
+};
 
-export default MealItemForm
+export default MealItemForm;
