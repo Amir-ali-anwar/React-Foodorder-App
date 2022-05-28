f778e559596cb0a74eff790504d67b61110f8f69
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-05-28 23:18:22 +0500

    working on cart item functionlaity

--- src/components/Meals/MealsItem/MealItemForm.js
+++ src/components/Meals/MealsItem/MealItemForm.js
@@ -2,10 +2,22 @@ import React from "react";
 import classes from "./MealItemForm.module.css";
 import Input from "../../UI/Input";
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = React.useState(true);
   const amountRef = React.useRef();
   const SubmitHandler = (e) => {
     e.preventDefault();
-    console.log("clicked");
+    const enteredAmount = amountRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+  
   };
   return (
     <form className={classes.form} onSubmit={SubmitHandler}>
@@ -21,6 +33,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
 };
